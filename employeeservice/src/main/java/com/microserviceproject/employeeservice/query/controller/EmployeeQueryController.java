c9fe029e805ac703275027d3ec05c7665e4d2ddb
sagiri <thang.nh225763@sis.hust.edu.vn>
2025-02-05 14:54:51 +0700

    config swagger information

--- employeeservice/src/main/java/com/microserviceproject/employeeservice/query/controller/EmployeeQueryController.java
+++ employeeservice/src/main/java/com/microserviceproject/employeeservice/query/controller/EmployeeQueryController.java
@@ -1,4 +1,47 @@
 package com.microserviceproject.employeeservice.query.controller;
 
+import com.microserviceproject.employeeservice.query.model.EmployeeResponseModel;
+import com.microserviceproject.employeeservice.query.queries.GetAllEmployeeQuery;
+import com.microserviceproject.employeeservice.query.queries.GetDetailEmployeeQuery;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/employees")
+@Tag(name = "Employee Query")
 public class EmployeeQueryController {
+	@Autowired
+	private QueryGateway queryGateway;
+
+	@Operation(
+			summary = "Get List Employee",
+			description = "Get endpoint for employee with filter",
+			responses = {
+					@ApiResponse(
+							description = "Success",
+							responseCode = "200"
+					),
+					@ApiResponse(
+							responseCode = "401",
+							description = "Unauthorized / Invalid Token"
+					)
+			}
+	)
+	@GetMapping
+	public List<EmployeeResponseModel> getAllEmployee(@RequestParam(required = false, defaultValue = "false") Boolean isDisciplined) {
+		return queryGateway
+				.query(new GetAllEmployeeQuery(isDisciplined), ResponseTypes.multipleInstancesOf(EmployeeResponseModel.class)).join();
+	}
+
+	@GetMapping("/{employeeId}")
+	public EmployeeResponseModel getDetailEmployee(@PathVariable String employeeId) {
+		return queryGateway.query(new GetDetailEmployeeQuery(employeeId), ResponseTypes.instanceOf(EmployeeResponseModel.class)).join();
+	}
 }
