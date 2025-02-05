2214b17a6552ed68da2bbc32464d3334c55ae641
sagiri <thang.nh225763@sis.hust.edu.vn>
2025-02-06 01:33:55 +0700

    done api gateway service

--- employeeservice/src/main/java/com/microserviceproject/employeeservice/query/controller/EmployeeQueryController.java
+++ employeeservice/src/main/java/com/microserviceproject/employeeservice/query/controller/EmployeeQueryController.java
@@ -6,6 +6,7 @@ import com.microserviceproject.employeeservice.query.queries.GetDetailEmployeeQu
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/employees")
 @Tag(name = "Employee Query")
+@Slf4j
 public class EmployeeQueryController {
 	@Autowired
 	private QueryGateway queryGateway;
@@ -36,6 +38,8 @@ public class EmployeeQueryController {
 	)
 	@GetMapping
 	public List<EmployeeResponseModel> getAllEmployee(@RequestParam(required = false, defaultValue = "false") Boolean isDisciplined) {
+		log.info("Calling to getAllEmployee");
+
 		return queryGateway
 				.query(new GetAllEmployeeQuery(isDisciplined), ResponseTypes.multipleInstancesOf(EmployeeResponseModel.class)).join();
 	}
