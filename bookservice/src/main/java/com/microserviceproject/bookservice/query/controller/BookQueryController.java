3510d91f420c76f9ffb39ea00461b5e208d0b8c4
sagiri <thang.nh225763@sis.hust.edu.vn>
2025-02-08 00:13:57 +0700

    initialize kafka for common service and test on book service and notification service

--- bookservice/src/main/java/com/microserviceproject/bookservice/query/controller/BookQueryController.java
+++ bookservice/src/main/java/com/microserviceproject/bookservice/query/controller/BookQueryController.java
@@ -3,13 +3,11 @@ package com.microserviceproject.bookservice.query.controller;
 import com.microserviceproject.bookservice.query.queries.GetBookDetailQuery;
 import com.microserviceproject.bookservice.query.model.BookResponseModel;
 import com.microserviceproject.bookservice.query.queries.GetAllBookQuery;
+import com.microserviceproject.commonservice.services.KafkaService;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +27,14 @@ public class BookQueryController {
 	public BookResponseModel getBookDetail(@PathVariable String bookId) {
 		GetBookDetailQuery query = new GetBookDetailQuery(bookId);
 		return queryGateway.query(query, ResponseTypes.instanceOf(BookResponseModel.class)).join();
+	}
+
+	@Autowired
+	private KafkaService kafkaService;
+
+	// test kafka
+	@PostMapping("/sendMessage")
+	public void sendMessage(@RequestBody String message) {
+		kafkaService.sendMessage("test", message);
 	}
 }
